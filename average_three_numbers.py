486fb73e2d303e4dafd25ac7356cc440b81ae8e0
bobsmichael <bobsmichael2811@gmail.com>
2025-08-07 09:54:43 +0700

    Add files via upload

---
+++ average_three_numbers.py
@@ -0,0 +1,5 @@
+num1 = float(input("Nhập số thứ nhất: "))
+num2 = float(input("Nhập số thứ hai: "))
+num3 = float(input("Nhập số thứ ba: "))
+average = (num1 + num2 + num3) / 3
+print(f"Trung bình của ba số là: {average}")
