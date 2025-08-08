bc73f458c74b0ebe0ade65f51748637a6908259b
bobsmichael <bobsmichael2811@gmail.com>
2025-08-08 17:40:23 +0700

    Add files via upload

---
+++ variables.py
@@ -0,0 +1,12 @@
+name = "Dương"
+age = 18
+height = 1.68
+is_student = True
+print("Tên:", name)
+print("Tuổi:", age)
+print("Chiều cao:", height)
+print("Là sinh viên:", is_student)
+age = age + 1
+print("Tuổi sau một năm:", age)
+year_of_birth = 2025 - age
+print("Năm sinh:", year_of_birth)
